0034a36f807cbeccdc1a51d9a9fa1f6ec95d64cd
bobo1234 <767062594@qq.com>
2017-10-17 10:10:35 +0800

    login1

---
+++ manager/src/com/java/back/test/MyTest.java
@@ -0,0 +1,33 @@
+package com.java.back.test;
+
+import java.io.Serializable;
+//import java.util.Iterator;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.java.back.redis.RedisSpringProxy;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring.xml")
+public class MyTest extends RedisSpringProxy{
+
+	
+	@Test
+	public void redis() {
+//		flushDB();
+		Set<Serializable> allKeys = getAllKeys();
+		for (Serializable serializable : allKeys) {
+			System.out.println(serializable);
+		}
+//		Long delete = delete("findMenu~keys");
+//		System.out.println(delete);
+		Object read = read("admin");
+		System.out.println(read);
+	}
+
+
+}
